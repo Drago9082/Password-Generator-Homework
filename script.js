a02a6da2ee4ae85fedd3df424ce26a28d4bc7d73
Drago9082 <maxtiwari9082@gmail.com>
2022-03-31 19:20:31 -0400

    added base function

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 ​
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function userSelected () {
+
+}
+
+function randomSelected () {
+
+}
+
+function generatePassword () {
+  
+}
